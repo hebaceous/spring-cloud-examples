9213a2f9c22a5f233727560b2a95c37436d7bcb2
舒友乐 <ylshu@xinshiyun.com>
2016-08-11 16:30:09 +0800

    eureka

---
+++ cloud-eureka/src/main/java/me/hebaceous/cloud/eureka/Eureka.java
@@ -0,0 +1,15 @@
+package me.hebaceous.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka.class, args);
+    }
+
+}
--- cloud-web/src/main/java/me/hebaceous/cloud/web/Web.java
+++ cloud-web/src/main/java/me/hebaceous/cloud/web/Web.java
@@ -2,8 +2,10 @@ package me.hebaceous.cloud.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Web {
 
     public static void main(String[] args) {
