84dcb2b8828ffda5852696f0c519226c18657444
Hebaceous <hebaceous@gmail.com>
2016-08-21 11:53:43 +0800

    boot:1.4 admin:1.4

---
+++ cloud-service-orders/src/test/java/me/hebaceous/cloud/order/AppTest.java
@@ -0,0 +1,21 @@
+package me.hebaceous.cloud.order;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void test() {
+
+    }
+}
