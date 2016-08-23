27b20f5b82168a0bbb2016622aaef2e949f62d7e
Hebaceous <hebaceous@gmail.com>
2016-08-23 13:58:00 +0800

    unit tests

--- cloud-service-orders/src/test/java/me/hebaceous/cloud/order/AppTest.java
+++ cloud-service-orders/src/test/java/me/hebaceous/cloud/order/AppTest.java
@@ -3,19 +3,24 @@ package me.hebaceous.cloud.order;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest({"eureka.client.enabled:false"})
+@AutoConfigureMockMvc
 public class AppTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void test() {
-
+    public void testAll() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andDo(MockMvcResultHandlers.print());
     }
 }
--- cloud-service-users/src/test/java/me/hebaceous/cloud/user/AppTest.java
+++ cloud-service-users/src/test/java/me/hebaceous/cloud/user/AppTest.java
@@ -3,12 +3,16 @@ package me.hebaceous.cloud.user;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest({"eureka.client.enabled:false"})
+@AutoConfigureMockMvc
 public class AppTest {
 
     @Autowired
@@ -16,6 +20,7 @@ public class AppTest {
 
     @Test
     public void testAll() throws Exception {
-
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andDo(MockMvcResultHandlers.print());
     }
 }
