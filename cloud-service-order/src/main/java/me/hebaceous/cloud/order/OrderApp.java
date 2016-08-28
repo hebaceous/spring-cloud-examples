7d2074c25fdef7f7ff55504a7bc9b00b8ec344ab
Hebaceous <hebaceous@gmail.com>
2016-08-28 13:38:26 +0800

    Polish user&order

--- cloud-service-order/src/main/java/me/hebaceous/cloud/order/OrderApp.java
+++ cloud-service-order/src/main/java/me/hebaceous/cloud/order/OrderApp.java
@@ -1,9 +1,11 @@
 package me.hebaceous.cloud.order;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringCloudApplication
+@SpringBootApplication
+@EnableDiscoveryClient
 public class OrderApp {
 
     public static void main(String[] args) {
